6dc47ff1bc4c5b66980e8e772979f7ef577d5154
Kaheem Freeman <kashfr@gmail.com>
2022-01-26 13:38:43 -0500

    making changes to REST_FRAMEWORK now

--- dcm_app/views.py
+++ dcm_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, redirect
 from rest_framework import viewsets, permissions, generics
+from rest_framework.response import Response
+from rest_framework import mixins
 from .serializers import MetaverseAvatarSerializer, CharacterSerializer
 from .models import MetaverseAvatar, Character
 
 # Create your views here.
+
+
+class MetaverseAvatarBookViewSet(viewsets.ModelViewSet):
+    queryset = MetaverseAvatar.objects.all()
+    serializer_class = MetaverseAvatarSerializer
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CharacterViewSet(viewsets.ModelViewSet):
+    queryset = Character
+    serializer_class = CharacterSerializer
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
