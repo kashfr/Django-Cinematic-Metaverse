e650d55ab42bc829f61b1912199127603774a7c4
Kaheem Freeman <kashfr@gmail.com>
2022-01-26 17:51:27 -0500

    fixed error with User Signup

--- dcm_accounts/views.py
+++ dcm_accounts/views.py
@@ -26,3 +26,25 @@ class LoginView(generics.ListCreateAPIView):
             serializer.is_valid()
             return Response(serializer.data)
         return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+class RegisterUsersView(generics.ListCreateAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username", "")
+        password = request.data.get("password", "")
+        email = request.data.get("email", "")
+        if not username or not password or not email:
+            return Response(
+                data={
+                    "message": "username, password and email is required to register a user"
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        new_user = User.objects.create_user(
+            username=username, password=password, email=email
+        )
+        return Response(status=status.HTTP_201_CREATED)
