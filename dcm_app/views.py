2b89b3c68f55ddb745b91889aec5160c4ea2c7fc
Kaheem Freeman <kashfr@gmail.com>
2022-01-26 21:53:06 -0500

    looking into why fields are not populating in some of my views

---
+++ dcm_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from rest_framework import viewsets, permissions, generics
+from rest_framework.response import Response
+from rest_framework import mixins
+from .serializers import MetaverseAvatarSerializer, CharacterSerializer
+from .models import MetaverseAvatar, Character
+
+# Create your views here.
+
+
+class MetaverseAvatarBookViewSet(viewsets.ModelViewSet):
+    queryset = MetaverseAvatar.objects.all()
+    serializer_class = MetaverseAvatarSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
+    # permissions_classes = [permissions.AllowAny]  # testing
+
+
+class CharacterViewSet(viewsets.ModelViewSet):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
+    # permissions_classes = [permissions.AllowAny]  # testing
