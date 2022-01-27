4821c0ea83b8ff507fc8e02d4ff64e2e35614809
Kaheem Freeman <kashfr@gmail.com>
2022-01-26 22:34:57 -0500

    working on deployment now

--- dcm_app/serializers.py
+++ dcm_app/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
-
 from .models import MetaverseAvatar, Character
 
 
 class MetaverseAvatarSerializer(serializers.ModelSerializer):
+    # characters = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = MetaverseAvatar
         fields = '__all__'
 
 
-class CharacterSerializer(serializers.HyperlinkedModelSerializer):
+class CharacterSerializer(serializers.ModelSerializer):
+    # avatars = serializers.HyperlinkedIdentityField(view_name='avatar-detail', read_only=True)
+
     class Meta:
         model = Character
         fields = '__all__'
