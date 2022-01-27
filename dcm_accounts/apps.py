22c642f1e765705f2a00cb82a08bab2cf770fa6d
Kaheem Freeman <kashfr@gmail.com>
2022-01-27 03:59:42 -0500

    got rid of duplicate files

--- dcm_accounts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DcmAccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dcm_accounts'
--- dcm_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import MetaverseAvatar, Character
-
-# Register your models here.
-admin.site.register(MetaverseAvatar)
-admin.site.register(Character)
-# admin.site.register([MetaverseAvatar, Character])
