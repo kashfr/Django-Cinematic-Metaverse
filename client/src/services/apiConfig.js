1b28ffa2d31886470499870cd1832c5fbd9626a2
Kaheem Freeman <kashfr@gmail.com>
2022-01-28 19:22:55 -0500

    fixed image links

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL:
-//     process.env.NODE_ENV === "production"
-//       ? "https://cinematic-metaverse.herokuapp.com/"
-//       : "http://localhost:8000/",
-// });
-
 const api = axios.create({
-  baseURL: "https://cinematic-metaverse.herokuapp.com/",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://cinematic-metaverse.herokuapp.com/"
+      : "http://localhost:8000/",
 });
+
+// const api = axios.create({
+//   baseURL: "https://cinematic-metaverse.herokuapp.com/",
+// });
 
 export default api;
